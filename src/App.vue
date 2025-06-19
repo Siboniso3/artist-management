a927ceb648deb7abaefd0ba26ff4a7e575469bd5
Siboniso3 <sibonisoncube9@gmail.com>
2025-06-19 15:14:34 +0200

    first ui vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Artist />
+</template>
+
+<script>
+
+import Artist from './components/Artist.vue'
+export default {
+  name: 'App',
+  components: {
+    Artist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  height: 1px;
+}
+</style>
---
+++ src/components/Artist.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <h1 >Event Organizer view</h1>
+        <h1 class ="text-center">Artist list</h1>
+        <table class="table table-striped">
+         <thead>
+         <th>Artist Id</th>
+         <th>Artist First Name</th>
+          <th>Artist Last Name</th>
+         <th>Stage Name</th>
+         <th>Genre</th>
+         <!-- <th>Location</th> -->
+         <!-- <th>Booking Status</th>  -->
+        </thead>
+        <tbody>
+            <tr v-for="artist in artists" v-bind:key="artist.id">
+                <td>{{artist.id}}</td>
+                <td>{{artist.fName}}</td>
+                <td>{{artist.lName}}</td>
+                <td>{{artist.stageName}}</td>
+                <td>{{artist.genre}}</td>
+                <!-- <td>{{artist.location}}</td> -->
+                <!-- <td>{{artist.status}}</td> -->
+            </tr>
+        </tbody>
+
+        </table>
+    </div>
+</template>
+
+<script>
+import ArtistService from '../assets/services/ArtistService'
+ 
+
+export default{
+    name :'Artists-list',
+    data(){
+        return{
+         artists : []
+        }
+      
+    },
+    methods:{
+     getArtists(){
+       ArtistService.getArtists().then((response) =>{
+        this.artists =response.data
+       });
+     }
+    },
+    created(){
+     this.getArtists()
+    }
+
+}
+</script>
+<style>
+ .container>div, h1 {
+  flex: 1;
+}
+</style>
